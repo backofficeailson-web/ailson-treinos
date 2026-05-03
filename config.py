dd5485e124d1666f58be6d0b814194378fabfa8b
backofficeailson-web <backofficeailson@gmail.com>
2026-05-03 11:21:50 -0300

    config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-# config.py
-
 DB_NAME = "ailson_personal.db"
+# Diretório para upload de fotos em disco
+UPLOAD_DIR = "uploads"
 
 # Cores
 VERDE_HULK = "#2ECC40"
@@ -15,6 +15,6 @@ BRANCO = "#FFFFFF"
 AMARELO_ALERTA = "#FFD700"
 VERMELHO = "#FF4136"
 
-# Autenticação
-USUARIO_VALIDO = "admin"
-SENHA_HASH = "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92"  # sha256 de "123"
+# Autenticação padrão (será usado apenas na criação do primeiro admin)
+ADMIN_USUARIO = "admin"
+ADMIN_SENHA_HASH = "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92"
